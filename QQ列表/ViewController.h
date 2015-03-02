d2bc70a49260365e06007e54a4d13e3fdcc8d22d
丁健 <Dj@dingjiandeAir.lan>
2015-03-02 19:45:02 +0800

    Initial Commit

---
+++ QQ列表/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQ列表
+//
+//  Created by 丁健 on 15/3/2.
+//  Copyright (c) 2015年 丁健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QQ列表/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QQ列表
+//
+//  Created by 丁健 on 15/3/2.
+//  Copyright (c) 2015年 丁健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
